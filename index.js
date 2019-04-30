3941207ca792775b829ce2d32f94bdea8b756b5a
Eric Aniag <ericaniag@gmail.com>
2019-04-30 21:49:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine, name) {
-  return "Welcome, " + `${name}` + ". You are number in line."
+  return "Welcome, " + `${name}` + ". You are number " + katzDeliLine.length + 1 " in line."
 }
 
 function nowServing(katzDeliLine) {
