1087770a5a971b443aa0fc6b9be35d0c945d660a
Eric Aniag <ericaniag@gmail.com>
2019-04-30 22:18:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    katzDeliLine.shift()
+    return katzDeliLine.shift()
   }
 }
 
