782eba06d92955276fd1008e7da05a6d7f59e89e
Eric Aniag <ericaniag@gmail.com>
2019-04-30 22:47:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
-    
-    return "The line is currently: " + 
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      return "The line is currently: " + i++ + katzDeliLine[i]
+    }
   }
 }
